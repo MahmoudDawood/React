182057b962a63b2caf8bd7f582943d7b8c9917f0
Mahmoud Dawood <mm.dawood16@yahoo.com>
2021-10-06 08:29:38 +0200

    Commit section 6

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env', 'react'],
+                    plugins: ['transform-class-properties']
+                }
+            }
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+}
