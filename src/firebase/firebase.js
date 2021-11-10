0f4ad253d4840500b24451c3ea06178bdbc89aa7
Mahmoud Dawood <mm.dawood16@yahoo.com>
2021-11-10 10:40:09 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import firebase from 'firebase'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-    apiKey: "AIzaSyDnJ2ToQ7lY8oJp_3Oj46osEHTkC9_HP1Y",
-    authDomain: "expensify-d1d60.firebaseapp.com",
-    databaseURL: "https://expensify-d1d60-default-rtdb.firebaseio.com",
-    projectId: "expensify-d1d60",
-    storageBucket: "expensify-d1d60.appspot.com",
-    messagingSenderId: "22554177404",
-    appId: "1:22554177404:web:993b9339e84ebb5beffef9",
-    measurementId: "G-GCX743NRQX"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASURMENT_ID,
 }
 
 // Initialize Firebase
@@ -18,20 +18,22 @@ firebase.initializeApp(config)
 
 const database = firebase.database()
 
+export { firebase, database as default }
+
 // child_removed
-database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val())
-})
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
 
-// child_changed
-database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val())
-})
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
 
-// child_added
-database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val())
-})
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
 
 // database.ref('expenses').on('value', (snapshot) => {
 //     const expenses = []
@@ -57,12 +59,12 @@ database.ref('expenses').on('child_added', (snapshot) => {
 //         console.log(expenses)
 //     })
 
-database.ref('expenses').push({
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0,
-})
+// database.ref('expenses').push({
+//     description: 'Gum',
+//     note: '',
+//     amount: 195,
+//     createdAt: 0,
+// })
 
 // database.ref('notes/-Mo3GqVz8eI2dgNOstYG').remove()
 
